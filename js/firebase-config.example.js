03ea794bb880f7be5fbb7a060c55c488b98b4ed6
Aryan Jaiswal <115605684+Aryan1234567899@users.noreply.github.com>
2025-12-07 01:06:40 +0530

    Update Firebase config with new project details
    Updated Firebase configuration with project-specific values.

--- js/firebase-config.example.js
+++ js/firebase-config.example.js
@@ -1,9 +1,10 @@
-// Copy this example into js/firebase-config.js and fill with your project's values
+
 export const firebaseConfig = {
-  apiKey: "AIza...",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyCwj4pRpeFZCBUdttrQYrKBFAQi_Kjd-m4",
+  authDomain: "intern-auth-project-45515.firebaseapp.com",
+  projectId: "intern-auth-project-45515",
+  storageBucket: "intern-auth-project-45515.firebasestorage.app",
+  messagingSenderId: "232291224442",
+  appId: "1:232291224442:web:8a6dca31c36b4731e364c3",
+  measurementId: "G-K7TWPK2D0V"
 };
